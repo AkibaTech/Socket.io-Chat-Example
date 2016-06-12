d2f4a7aa09434128a87c83932d34593772d93e34
MarceauKa <marceau@casals.fr>
2016-06-12 20:01:45 +0200

    Typo, comment et ajustement demande du nom d'utilisateur

--- index.js
+++ index.js
@@ -2,30 +2,40 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+// Petite config...
 var config = {
-    limit: 3
+    limit: 3 // Nombre maximum de participants
 };
 
+// Stockes les utilisateurs connectés
 var users = [];
 
+// Lance l'écoute du serveur sur le port 3000
 server.listen(3000, function(){
   console.log('Le serveur a démarré sous *:3000');
 });
 
+// Envoie le code client au navigateur
 app.get('/', function(request, response) {
     response.sendFile(__dirname + '/index.html');
 });
 
+// Connection socket.io
 io.on('connection', function(socket) {
 
+    // Le chat est complet...
     if (Object.keys(io.sockets.sockets).length > config.limit) {
+        // Envoi le message au client et le déconnecte
         socket.emit('chat.full');
         socket.disconnect();
     }
 
+    // Envoi la demande de nom d'utilisateur
     socket.emit('chat.username');
 
+    // Envoi l'évenement de nouvel utilisateur
     socket.on('chat.new', function(username) {
+        // Le nom d'utilisateur n'est pas déjà pris
         if (users.indexOf(username) === -1) {
             users.push(username);
             socket.username = username;
@@ -37,7 +47,9 @@ io.on('connection', function(socket) {
         }
     })
 
+    // Réception d'un nouveau message
     socket.on('chat.message', function(data) {
+        // On l'envoi à tout le monde
         io.emit('chat.message', {
             'username': socket.username,
             'message': data.message,
@@ -45,9 +57,13 @@ io.on('connection', function(socket) {
         });
     });
 
+    // Un gars s'est déconnecté...
     socket.on('disconnect', function() {
+        // Libère son nom d'utilisateur
         var index = users.indexOf(socket.username);
         users.splice(index, 1);
+
+        // Envoie la mise à jour des utilisateurs connectés
         io.emit('chat.new', users);
     });
 });
